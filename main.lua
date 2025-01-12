5ff09f82218ab1f09d115ac62b536d08eadf5b78
capped-_- <166282207+capped-uwu@users.noreply.github.com>
2025-01-12 19:00:04 +0900

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,8 @@
+-- wploits hub script
+local placeId = game.PlaceId
+
+if placeId == 16732694052 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/capped-uwu/wploitshub/refs/heads/main/fisch.lua"))()
+else
+  warn("unsuported game")
+end
