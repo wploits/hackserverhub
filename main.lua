6869ed04ce98a7e1b773d7dcda5ec907e9d89cad
capped-_- <166282207+capped-uwu@users.noreply.github.com>
2025-01-12 19:03:31 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,7 @@
--- wploits hub script
 local placeId = game.PlaceId
 
 if placeId == 16732694052 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/capped-uwu/wploitshub/refs/heads/main/fisch.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/capped-uwu/hackserverhub/refs/heads/main/fisch.lua"))()
 else
   warn("unsuported game")
 end
