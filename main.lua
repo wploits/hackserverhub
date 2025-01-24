72ee8f5033410a424349b02661b0d6b4a8050098
capped-_- <166282207+capped-uwu@users.noreply.github.com>
2025-01-24 21:36:23 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ local placeId = game.PlaceId
 
 if placeId == 16732694052 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/capped-uwu/hackserverhub/refs/heads/main/fisch.lua"))()
+elseif placeId == 6961824067 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/capped-uwu/hackserverhub/refs/heads/main/ftap.lua"))()
 else
   warn("unsuported game")
 end
